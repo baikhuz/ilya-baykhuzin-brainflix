7c5be3b8a04cb3de14be316c03d997290406c143
Ilya Baykhuzin <ilyabaykhuzin@DESKTOP-RS2G0E0.localdomain>
2019-05-01 17:22:50 -0400

    initial HTML renders, created sass structure

---
+++ sprint-1/src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from '../../assets/Logo/Logo-brainflix.svg';
+import './Header.scss';
+
+
+class Header extends Component {
+
+    clickHandler = (e) => {
+        e.preventDefault();
+    }
+
+    render () {
+        return (
+            <header className = "header">
+                <div className = "header__logo--container">
+                    <img className = "logo" src={logo} alt="Brainflix Logo"/>
+                </div>
+                <form className = "header__searchbar">
+                    <button className = "header__searchbar--button" type = "submit" onClick={e => this.clickHandler(e)}></button>
+                    <input className = "header__searchbar--input" type = "text" placeholder="Search"/>
+                </form>
+                <div className="header__upload">
+                    <button className="header__upload--button" type = "button"></button>
+                    <div className="user-avatar header__upload--user-avatar"></div>
+                </div>
+            </header>
+        );
+    }
+}
+export default Header;
